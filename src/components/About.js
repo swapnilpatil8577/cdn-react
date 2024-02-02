1c08a813830579e9b3983e2e263c15d2b10d4cef
Swapnil Patil <spatil.encora@centricsoftware.com>
2024-02-02 13:35:02 +0530

    Assignment8-Class based Component

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,27 @@
-const About = () => {
-    return (
-        <div>
-            <h1>About</h1>
-            <h2>This is Namaste React Web Series</h2>
-        </div>
-    )
+import User from "./User";
+import UserClass from "./UserClass";
+import { Component } from "react";
+class About extends Component {
+    constructor(props) {
+        super(props)
+
+        // console.log("Parent constructor call")
+    }
+
+    componentDidMount() {
+        // console.log("Parent component did mount call")
+    }
+    render() {
+        // console.log("Parent render call")
+        return (
+            <div>
+                <h1>About</h1>
+                {/* <User name="Swapnil Patil"/> */}
+                <UserClass name="Swapnil Patil Class" location="Kolhapur Class"/>
+                <h2>This is Namaste React Web Series</h2>
+            </div>
+        )        
+    }
 }
 
 export default About;
